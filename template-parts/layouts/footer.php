c7c9d9685cfaf0e25b2a0c4f822671ffce1a9c11
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-11-07 21:26:37 +0700

    feat: add footer

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,8 +1,33 @@
     <!--footer-->
     <div class="footer">
         <div class="container">
-            <div class="grid">
-                
+            <div class="grid uk-grid-10 uk-grid-20-m">
+                <div class="width-1-3-m">
+                    <h3 class="footer__title">BAN CHỈ ĐẠO VỀ QUẢN LÝ VỆ SINH AN TOÀN THỰC PHẨM TỈNH THANH HÓA</h3>
+                    <h3 class="footer__title">VĂN PHÒNG ĐIỀU PHỐI VỀ VỆ SINH AN TOÀN THỰC PHẨM</h3>
+
+                    <p class="footer__desc">Nội dung thông tin trên được quản lý và giám sát bởi cơ quan Nhà nước có thẩm quyền</p>
+
+                    <p class="footer__desc">Cấm mọi hình thức khai thác trái phép khi chưa có sự đồng ý của đơn vị chủ quản.</p>
+
+                    <p class="footer__desc">HỆ THỐNG ĐANG CHẠY THỬ NGHIỆM</p>
+                </div>
+                <div class="width-1-3-m">
+                    <h3 class="footer__title">DANH MỤC</h3>
+                    <ul class="footer__list">
+                        <li><a href="">Sản phẩm|Đơn vị phân phối</a></li>
+                        <li><a href="">Chuỗi thực phẩm an toàn</a></li>
+                        <li><a href="">Giới thiệu|Trợ giúp</a></li>
+                    </ul>
+                </div>
+                <div class="width-1-3-m">
+                    <h3 class="footer__title">ĐƯỜNG DÂY NÓNG TIẾP NHẬN TIN PHẢN ẢNH VI PHẠM ATTP</h3>
+                    <div class="footer__desc">
+                        <div>02373961818</div>
+                        <div>vpdpattp@thanhhoa.gov.vn</div>
+                        <div>35 đại lộ Lê Lợi, phường Lam Sơn, TP Thanh Hóa, tỉnh Thanh Hóa</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
